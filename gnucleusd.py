fecc0b46de26afdb6f9cec6f679c82594a71ee9b
Kris Absinthe <krisabsinthe42@gmail.com>
2015-03-03 17:56:05 -0500

    fixed text alerts into a dictionary and adding call detection

--- gnucleusd.py
+++ gnucleusd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
+# This was developed with the SIM900 chip
 import serial, time, sys
-
 line = []
 ser = serial.Serial(
     port='/dev/ttyACM0',
@@ -12,25 +12,61 @@ ser = serial.Serial(
 
 GsmAT = "AT"
 GsmSetFunc = "AT+CFUN=1"
+GsmSetCRC = "AT+CRC=1" 
+GsmIncomingCall = "+CRING" 
+GsmSetCallerID = "AT+CLIP=1"
+GsmSetAlert = "AT+CALS=1"
+smsDeleteMemoryBlock="AT+CMGD="
+smsReceived = "+CMTI: \"SM\"" 
+smsRead = "AT+CMGR="
+smsUnreadMessage = "+CMGR: \"REC UNREAD\""
 
 def GsmModemSendCommand(GsmCommand):
     ser.write(GsmCommand + "\r")
     time.sleep(0.25) 
 
 def GsmModem_init ():
+     print("Sending modem init sequence")
      GsmModemSendCommand(GsmAT)
      GsmModemSendCommand(GsmSetFunc)
+     GsmModemSendCommand(GsmSetCRC)
+     GsmModemSendCommand(GsmSetCallerID)
+     GsmModemSendCommand(GsmSetAlert)
+
+def GsmDeleteSMS(blockID):
+    # dont fill up SMS blocks
+    GsmModemSendCommand(blockID)
+    
+
+def SmsGetFromMemory(blockID):
+    smsDict = {}
+    GsmModemSendCommand(smsRead + blockID)   
+    smsModemCommand = ser.readline()
+    smsHeader = ser.readline()
+    smsMessage = ser.readline()
+    smsMessage = smsMessage.strip("\r\n")
+    smsHeaderParse = smsHeader.split(',')
+    smsSender = smsHeaderParse[1]
+    smsSender = smsSender.strip('"')
+    smsDict = {smsSender:smsMessage}
+    print smsDict
 
 GsmModem_init()
-
-if sys.argv[1] == "--command" or sys.argv[1] == "-c": 
-    #send generic command to modem
-    GsmModemSendCommand(sys.argv[2])
-    exit(0)
 
 while True:
     data = ser.readline()
     if len(data) > 0:
-        print(data)    
-    time.sleep(0.5)
+        if smsReceived in data:
+            data = data.strip('\r\n')
+            data = data.strip('\r')
+            data = data.strip('\n')
+            data = data.split(',',1)
+            blockID = data[1]
+            SmsGetFromMemory(blockID)
+            GsmDeleteSMS(blockID)
+     
+        if GsmIncomingCall in data:
+            GsmCaller = ser.readline()     
+            print GsmCaller 
+   
     
